f3c2c5dbc4f406c9d94002ef50a64a49a6ddd73f
Морозов Евгений <evgenii.morozov@sparm.com>
2025-03-02 17:33:17 +0300

    test

--- HW 2/ViewController.swift
+++ HW 2/ViewController.swift
@@ -23,14 +23,17 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dispersion()
+        
 //        self.navigationItem.hidesBackButton = true
         
         colorView.layer.cornerRadius = 15
 //        setColor()
         
-        redLabel.text = string(from: redSlider)
-        greenLabel.text = string(from: greenSlider)
-        blueLabel.text = string(from: blueSlider)
+//        redLabel.text = string(from: redSlider)
+//        greenLabel.text = string(from: greenSlider)
+//        blueLabel.text = string(from: blueSlider)
+        dispersion()
     }
     
     @IBAction private func sliderAction(_ sender: UISlider) {
@@ -58,6 +61,28 @@ final class ViewController: UIViewController {
     
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
+    }
+    
+    private func dispersion() {
+        if let color = colorView.backgroundColor {
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+
+            color.getRed(&red,
+                         green: &green,
+                         blue: &blue,
+                         alpha: &alpha)
+            
+            redSlider.value = Float(red)
+            greenSlider.value = Float(green)
+            blueSlider.value = Float(blue)
+            
+            redLabel.text = "\(red)"
+            greenLabel.text = "\(green)"
+            blueLabel.text = "\(blue)"
+        }
     }
 }
 
